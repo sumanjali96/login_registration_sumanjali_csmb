c0470b7798e19c1e309743c9e697047ec1d9b392
sumanjali96 <sumanjali.chapala24@sasi.ac.in>
2026-07-14 10:26:54 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const connectDB = async () => {
   try {
+    // MongoDB Atlas connection string
+    const connString = "mongodb+srv://KulsumShaik24_db_user:kulsum20@cluster0.nxftcfq.mongodb.net/kulsum?appName=cluster0";
     
-    await mongoose.connect('mongodb://localhost:27017/workshop_db');
-    
+    await mongoose.connect(connString);
     console.log('MongoDB connected successfully!');
   } catch (err) {
     console.error('Database connection error:', err.message);
